222e1b43a64dcfc730eaee5b56ceff08c93b1d82
WestonJN <westonnyirenda@gmail.com>
2020-01-14 16:25:09 +0200

    Added specs

---
+++ spec/test.spec.js
@@ -0,0 +1,10 @@
+// let visitor = require('../src/node.js')
+// describe('function save()', () => {
+//     let alice = new visitor.Visitor('alice_cooper', 21, '13/01/2020', '12:00', 'amazing', 'Sbonelo')
+//     it('exists', () => {
+//         expect(alice.save()).toBeDefined()
+//     })
+//     // it('creates named files', () => {
+//     //     expect(alice.save()).to
+//     // })
+// })
---
+++ src/node.js
@@ -0,0 +1,45 @@
+'use strict';
+const fs = require('fs');
+
+class Visitor {
+    constructor(Name, Age, Visit_date, Time, Comments, Assistant) {
+        this.fullName = Name;
+        this.Age = Age;
+        this.dateOfVisit = Visit_date;
+        this.timeOfVisit = Time;
+        this.comments = Comments;
+        this.assistant = Assistant;
+    }
+    
+    async save() {
+            let file = this.fullName.replace(' ', '_').toLowerCase();
+            fs.writeFile(`visitor_${file}.json`, JSON.stringify(this,null,2) , function (err) {
+           if (err) throw err;
+       });
+       return(this);
+    }
+
+}
+
+
+    async function load(fullName){
+        let file = fullName.replace(' ', '_').toLowerCase();
+
+        await fs.readFile(`visitor_${file}.json`, (err, data) => {
+            if (err) throw err;
+            console.log(data.toString());
+        });
+    }
+
+    let alice = new Visitor('Alice Smith', 12, '12/03/2019', '13:26', 'yey!', 'Weston')
+    alice.save();
+    load("Alice Smith");
+
+    let bob = new Visitor('Bob Marley', 26, '1/06/2020', '08:26', 'Boring', 'Mpumelelo')
+    bob.save();
+    load("Bob Marley");
+
+    let charlie = new Visitor('Charlie Chaplin', 91, '16/09/2019', '10:15', 'Yeeeeeeeeeeeey!!!!', 'Tadiwa')
+    charlie.save();
+    load("Charlie Chaplin");
+
